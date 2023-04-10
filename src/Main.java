080c0ac676fb6c536d94cfd3ab380f0e41bd2707
Dima Bolushev <bdima3003@mail.ru>
2023-04-10 19:55:55 +0300

    twoCommit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.beans.PropertyEditorSupport;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        BonusMilesService bonus = new BonusMilesService();
+        int yourBonus=bonus.calculate(10_000);
+        System.out.println("Спасибо за покупку, мы начислили вам " + yourBonus + " милей");
     }
 }
